fd79b01b5fc646e87d653e11a15b48dcae17af30
Kirill <kovalev.kir.dmitr@gmail.com>
2023-08-15 18:13:04 +0300

    add locators

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,7 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
     REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')
+
+class ProductPageLocators:
+    BUTTON_ADD_BASKET = (By.CSS_SELECTOR, '.btn-add-to-basket')
+    BASKET_ADDED_NAME = (By.CSS_SELECTOR, '')

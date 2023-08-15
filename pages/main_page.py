207c8f55075796fb70a07c43bdc16989679ca27b
Kirill <kovalev.kir.dmitr@gmail.com>
2023-08-15 12:56:03 +0300

    Update

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,4 +1,4 @@
-# Главная Страница
+# Главная Страница|Методы тестов
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
 
@@ -8,4 +8,4 @@ class MainPage(BasePage):
         self.login_link.click()
 
     def should_be_login_link(self):  # проверить ссылку на логин
-        self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), 'Login link is not presented'

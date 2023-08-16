2387986e68588a26dab27bcb4344e62d845c090c
Kirill <kovalev.kir.dmitr@gmail.com>
2023-08-16 17:43:56 +0300

    update url

--- test_product_page.py
+++ test_product_page.py
@@ -3,7 +3,7 @@ import time
 from pages.product_page import ProductPage
 
 def test_guest_can_add_product_to_basket(browser):
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
     page = ProductPage(browser, link)  # инициализируем объект страницу
     page.open()                        # открываем страницу
     page.add_basket()                  # добавляем товар в корзину

41626e144d0784f8304b1001bb70ea7d1798f8ad
Kirill <kovalev.kir.dmitr@gmail.com>
2023-08-16 14:12:39 +0300

    add def

--- pages/product_page.py
+++ pages/product_page.py
@@ -3,10 +3,18 @@ from .base_page import BasePage
 from .locators import ProductPageLocators
 
 class ProductPage(BasePage):
-    def add_basket(self):
+    def add_basket(self):  # добавить в корзину
         self.button_add_basket = self.browser.find_element(*ProductPageLocators.BUTTON_ADD_BASKET)
         self.button_add_basket.click()
 
-    def should_be_basket_added_name(self):
-        self.basket_added_name = self.browser.find_element()
-        assert 
+    def match_basket_added_name(self):  # проверка: "Название товара в корзине"
+        self.name_added_basket = self.browser.find_element(*ProductPageLocators.NAME_ADDED_BASKET)
+        self.name_product = self.browser.find_element(*ProductPageLocators.NAME_PRODUCT)
+        assert self.name_product.text == self.name_added_basket.text, \
+            'product name not match basket product name'
+
+    def match_basket_added_price(self):
+        self.price_product = self.browser.find_element(*ProductPageLocators.PRICE_PRODUCT)
+        self.price_added_basket = self.browser.find_element(*ProductPageLocators.PRICE_ADDED_BASKET)
+        assert self.price_product.text == self.price_added_basket.text, \
+            'product price not match basket product price'

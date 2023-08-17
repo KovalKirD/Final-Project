18a9d957500282e769a05973bd690cdceeb1f036
Kirill <kovalev.kir.dmitr@gmail.com>
2023-08-17 14:31:44 +0300

    Update base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,6 +2,9 @@
 import math
 from selenium.common import NoSuchElementException
 from selenium.common import NoAlertPresentException
+from selenium.common import TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 class BasePage:
     def __init__(self, browser, url, timeout=10):  # конструктор класса
@@ -12,10 +15,25 @@ class BasePage:
     def open(self):  # открывает страницу
         self.browser.get(self.url)
 
-    def is_element_present(self, how, what):  # перехватчик исключений
+    def is_element_present(self, how, what):  # перехватчик исключений: элемент представлен
         try:
             self.browser.find_element(how, what)
         except NoSuchElementException:
+            return False
+        return True
+
+    def is_not_element_present(self, how, what, timeout=4):  # перехватчик исключений: элемент отсутствует
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+        return False
+
+    def is_disappeared(self, how, what, timeout=4):  # перехватчик исключений: элемент исчез
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException). \
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
             return False
         return True
 

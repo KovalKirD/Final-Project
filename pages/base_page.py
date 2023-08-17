fb959ca0d87dff5a5c8ea0c8885d16dbc596b86a
Kirill <kovalev.kir.dmitr@gmail.com>
2023-08-17 16:31:23 +0300

    add def

--- pages/base_page.py
+++ pages/base_page.py
@@ -5,12 +5,20 @@ from selenium.common import NoAlertPresentException
 from selenium.common import TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from .locators import BasePageLocators
 
 class BasePage:
     def __init__(self, browser, url, timeout=10):  # конструктор класса
         self.browser = browser                 # назначить браузер
         self.url = url                         # присвоить ссылку
         self.browser.implicitly_wait(timeout)  # неявное ожидание элемента
+
+    def go_to_login_page(self):  # перейти на страницу авторизации
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK_INVALID)
+        link.click()
+
+    def should_be_login_link(self):  # проверить ссылку на логин
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), 'Login link is not presented'
 
     def open(self):  # открывает страницу
         self.browser.get(self.url)

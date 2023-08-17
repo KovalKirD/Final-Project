0f4a59b79e2d07fdfc851a27d35d129140f99d73
Kirill <kovalev.kir.dmitr@gmail.com>
2023-08-17 16:31:38 +0300

    add locators

--- pages/locators.py
+++ pages/locators.py
@@ -1,8 +1,12 @@
 # Локаторы
 from selenium.webdriver.common.by import By
 
+class BasePageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+
 class MainPageLocators:
-    LOGIN_LINK = (By.CSS_SELECTOR, '#login_link')
+    pass
 
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
@@ -12,6 +16,6 @@ class ProductPageLocators:
     BUTTON_ADD_BASKET = (By.CSS_SELECTOR, '.btn-add-to-basket')  # кнопка: "Добавить в Корзину"
     NAME_ADDED_BASKET = (By.CSS_SELECTOR, '#messages > div:nth-child(1) > div > strong')  # название: "Товар добавленный в козину"
     NAME_PRODUCT = (By.CSS_SELECTOR,'div.col-sm-6.product_main > h1')  # название: "Товар"
-    PRICE_PRODUCT = (By.CSS_SELECTOR, '.product_main > p.price_color') # цена товара
-    PRICE_ADDED_BASKET = (By.CSS_SELECTOR, '.in > div > p:nth-child(1) > strong')  # цена товара добаленного в корзину
-    SUCCESS_MASSAGE = (By.CSS_SELECTOR, '#messages > div:nth-child(1)')  # успешное сообщение
+    PRICE_PRODUCT = (By.CSS_SELECTOR, '.product_main > p.price_color')  # текст: цена товара
+    PRICE_ADDED_BASKET = (By.CSS_SELECTOR, '.in > div > p:nth-child(1) > strong')  # текст: цена товара добаленного в корзину
+    SUCCESS_MASSAGE = (By.CSS_SELECTOR, '#messages > div:nth-child(1)')  # элемент: успешное сообщение

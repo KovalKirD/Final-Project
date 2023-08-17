a9550ebba662f31f01f401dfc96f6012ec8677d0
Kirill <kovalev.kir.dmitr@gmail.com>
2023-08-17 14:31:55 +0300

    Update product_page.py

--- pages/product_page.py
+++ pages/product_page.py
@@ -11,10 +11,18 @@ class ProductPage(BasePage):
         self.name_added_basket = self.browser.find_element(*ProductPageLocators.NAME_ADDED_BASKET)
         self.name_product = self.browser.find_element(*ProductPageLocators.NAME_PRODUCT)
         assert self.name_product.text == self.name_added_basket.text, \
-            'product name not match basket product name'
+            'Product name not match basket product name'
 
-    def match_basket_added_price(self):
+    def match_basket_added_price(self):  # проверка: "Цена товара в корзине"
         self.price_product = self.browser.find_element(*ProductPageLocators.PRICE_PRODUCT)
         self.price_added_basket = self.browser.find_element(*ProductPageLocators.PRICE_ADDED_BASKET)
         assert self.price_product.text == self.price_added_basket.text, \
-            'product price not match basket product price'
+            'Product price not match basket product price'
+
+    def should_not_be_success_message(self):  # проверка: "Успешное сообщение"
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MASSAGE), \
+            'Success message is presented, but should not be'
+
+    def should_is_disappeared_success_message(self):  # проверка: "Элемент исчез"
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MASSAGE), \
+            'Success message is not disappeared, but should is disappeared'

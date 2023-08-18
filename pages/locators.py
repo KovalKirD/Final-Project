35387b2354f6d6bcdcc428fc06fe12143f523a33
Kirill <kovalev.kir.dmitr@gmail.com>
2023-08-18 13:12:40 +0300

    add locators basket

--- pages/locators.py
+++ pages/locators.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 class BasePageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    VIEW_BASKET = (By.CSS_SELECTOR, 'div.basket-mini.pull-right.hidden-xs a')
 
 class MainPageLocators:
     pass
@@ -18,4 +19,8 @@ class ProductPageLocators:
     NAME_PRODUCT = (By.CSS_SELECTOR,'div.col-sm-6.product_main > h1')  # название: "Товар"
     PRICE_PRODUCT = (By.CSS_SELECTOR, '.product_main > p.price_color')  # текст: цена товара
     PRICE_ADDED_BASKET = (By.CSS_SELECTOR, '.in > div > p:nth-child(1) > strong')  # текст: цена товара добаленного в корзину
-    SUCCESS_MASSAGE = (By.CSS_SELECTOR, '#messages > div:nth-child(1)')  # элемент: успешное сообщение
+    SUCCESS_MASSAGE = (By.CSS_SELECTOR, '#messages > div:nth-child(1)')  # сообщение: успешное сообщение
+
+class BasketPageLocators:
+    MESSAGE_EMPTY_BASKET = (By.CSS_SELECTOR, '#content_inner > p')  # сообщение: 'Your basket is empty.'
+    MESSAGE_ITEMS_BY_BASKET = (By.CSS_SELECTOR, 'div.basket-title.hidden-xs h2')  # сообщение: 'Items to buy now'

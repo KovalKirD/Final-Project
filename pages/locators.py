f036b7baa6f0b30126ac61574479d7ab8feb3376
Kirill <kovalev.kir.dmitr@gmail.com>
2023-08-19 10:34:06 +0300

    add locators for register

--- pages/locators.py
+++ pages/locators.py
@@ -5,6 +5,7 @@ class BasePageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
     VIEW_BASKET = (By.CSS_SELECTOR, 'div.basket-mini.pull-right.hidden-xs a')
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 class MainPageLocators:
     pass
@@ -12,6 +13,10 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
     REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')
+    REGISTER_EMAIL = (By.ID, 'id_registration-email')
+    REGISTER_PASSWORD = (By.ID, 'id_registration-password1')
+    REGISTER_PASSWORD_CONFIRM = (By.ID, 'id_registration-password2')
+    BUTTON_REGISTER = (By.CSS_SELECTOR, '#register_form button')
 
 class ProductPageLocators:
     BUTTON_ADD_BASKET = (By.CSS_SELECTOR, '.btn-add-to-basket')  # кнопка: "Добавить в Корзину"
